fa836357ce36286cc604f7b1820609d04459e924
abhimanyurai <84062253+abhimanyurai@users.noreply.github.com>
2023-12-12 12:47:40 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -37,7 +37,8 @@ from llama_index.vector_stores import ChromaVectorStore
 import streamlit as st
 import streamlit_authenticator as sa
 
-app_path = "D:/dev/maintenance_app"
+#app_path = "D:/dev/maintenance_app"
+#app_path = "."
 logo_path = app_path+"/Files/logo.png"
 
 
