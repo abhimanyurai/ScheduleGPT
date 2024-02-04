5de14f69d2bb4e803c6b2fe8202ace878467eb83
abhimanyurai <84062253+abhimanyurai@users.noreply.github.com>
2024-02-04 20:06:58 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -237,6 +237,7 @@ def my_custom_function(query):
     return llm.query(query)
 
 llm_query_engine = FunctionTool.from_defaults(fn=my_custom_function)
+
 if uploaded_file:
     df = load_data(uploaded_file)
     
@@ -260,7 +261,8 @@ if uploaded_file:
     
     # Given a query, this query engine `SubQuestionQueryEngine ` will generate a “query plan”
     # containing sub-queries against sub-documents before synthesizing the final answer.
-    s_engine = SubQuestionQueryEngine.from_defaults(query_engine_tools=query_engine_tools, verbose=True)
+    #s_engine = SubQuestionQueryEngine.from_defaults(query_engine_tools=query_engine_tools, verbose=True)
+    agent = OpenAIAgent.from_tools(query_engine_tools,llm=llm verbose=True)
     
     
     # response = s_engine.query("Which machine shows the highest equipment breakdown? Give me 2-3 recommendations for its maintenance")
@@ -281,7 +283,7 @@ if uploaded_file:
     if prompt is not None:
         with st.chat_message("Schedule Bot"):
                 try: 
-                    response = s_engine.query(prompt)
+                    response = agent.chat(prompt)
                 except KeyError as k:
                     st.session_state.messages.append({"role": "Schedule Bot", "content": "Can you please reframe the question"})
                     st.write("Can you please reframe the question")
