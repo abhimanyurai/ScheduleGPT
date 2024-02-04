352665b284531b6a9cd3008b900185500e7ea04a
abhimanyurai <84062253+abhimanyurai@users.noreply.github.com>
2024-02-04 20:42:52 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -233,7 +233,7 @@ uploaded_file = st.file_uploader(
 
 
 st.markdown('---')
-llm = OpenAI(temperature=0,model="gpt-3.5-turbo-0613")
+llm = OpenAI(model="gpt-3.5-turbo-0613")
 def my_custom_function(query):
     return llm.query(query)
 
